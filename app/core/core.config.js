9fd9b209c8d8c62f5f80d7c62d3ccccb8bb5af79
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-06-22 23:57:45 +0300

    first commit

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -2,11 +2,7 @@
     angular
         .module('app')
         .config(mainConfig);
-    // .config(['$mdIconProvider', function ($mdIconProvider) {
-    //     $mdIconProvider
-    //         .iconSet('social', 'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content-symbol.svg', 24)
-    //         .defaultIconSet('bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content-symbol.svg', 24);
-    // }]);
+
 
     mainConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 
@@ -15,7 +11,6 @@
 
         $urlRouterProvider.otherwise('/home');
 
-        // $locationProvider.html5Mode(true);
 
         $stateProvider
 
@@ -24,7 +19,8 @@
                 templateUrl: 'templates/homepage/homepage.html',
                 controller: 'HomepageController',
                 controllerAs: 'vm',
-            });
+            })
+            // $locationProvider.html5Mode(true);
 
 
     }
