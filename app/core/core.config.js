cc93ff8ed05465650fb4aea74b6476471520e681
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-06-25 19:01:39 +0300

    add all solution

--- app/core/core.config.js
+++ app/core/core.config.js
@@ -18,9 +18,59 @@
                 url: '/home',
                 templateUrl: 'templates/homepage/homepage.html',
                 controller: 'HomepageController',
-                controllerAs: 'vm',
+                controllerAs: 'vm'
             })
-            // $locationProvider.html5Mode(true);
+            .state('ecn', {
+            url: '/ecn',
+            templateUrl: 'templates/ecn/ecn.html',
+            controller: 'ECNController',
+            controllerAs: 'vm'
+            })
+            .state('mt4', {
+                url: '/mt4',
+                templateUrl: 'templates/mt4/mt4.html',
+                controller: 'MT4Controller',
+                controllerAs: 'vm'
+            })
+            .state('liquidity', {
+                url: '/liquidity',
+                templateUrl: 'templates/liquidity/liquidity.html',
+                controller: 'LiquidityController',
+                controllerAs: 'vm'
+            })
+            .state('trade', {
+                url: '/trade',
+                templateUrl: 'templates/trade/trade.html',
+                controller: 'TradeController',
+                controllerAs: 'vm'
+            })
+            .state('provider', {
+                url: '/provider',
+                templateUrl: 'templates/provider/provider.html',
+                controller: 'ProviderController',
+                controllerAs: 'vm'
+            })
+            .state('floating', {
+                url: '/floating',
+                templateUrl: 'templates/floating/floating.html',
+                controller: 'FloatingController',
+                controllerAs: 'vm'
+            })
+            .state('auto_risk', {
+                url: '/auto_risk',
+                templateUrl: 'templates/auto_risk/auto_risk.html',
+                controller: 'AutoRiskController',
+                controllerAs: 'vm'
+            })
+            .state('api', {
+                url: '/api',
+                templateUrl: 'templates/api/api.html',
+                controller: 'APIController',
+                controllerAs: 'vm'
+            })
+
+
+        // $locationProvider.html5Mode(true);
 
 
     }
--- app/templates/homepage/homepage.controller.js
+++ app/templates/homepage/homepage.controller.js
@@ -8,38 +8,56 @@
     HomepageController.$inject = [];
 
     function HomepageController() {
-        let vm = this;
+        var vm = this;
 
         vm.solutionList = [
             {
                 image:'content/img/homepage_solution/1.png',
-                title:'lorem ipsum',
-                text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at corporis dignissimos facilis',
-            },
-            {
-                image:'content/img/homepage_solution/2.png',
-                title:'lorem ipsum dolor',
-                text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at corporis dignissimos facilis inventore, libero '
-            },
-            {
-                image:'content/img/homepage_solution/3.png',
-                title:'lorem ipsum dol',
-                text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at corporis dignissimos facilis inventore, libero non perferendis quis sapiente velit?'
+                title:'ECN',
+                text:'A high-tech multi-module product which serves as the basis for the whole system',
+                link:'ecn'
             },
             {
                 image:'content/img/homepage_solution/4 (2).png',
-                title:'lorem ip',
-                text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+                title:'MT4/MT5/API Bridge',
+                text:'Highly functional bridges – the ideal solution for all brokers.',
+                link:'mt4'
+            },
+            {
+                image:'content/img/homepage_solution/2.png',
+                title:'Liquidity Solutions',
+                text:'We offer several options for obtaining high-quality liquidity',
+                link:'liquidity'
+            },
+            {
+                image:'content/img/homepage_solution/3.png',
+                title:'Trade Settings',
+                text:'A wide range of features which provide customers with the ability to customise order execution',
+                link:'trade'
             },
             {
                 image:'content/img/homepage_solution/5 (2).png',
-                title:'lorem ipsum dolor jhfdnf',
-                text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at '
+                title:'Provider management',
+                text:'Automatic elimination of imbalances in liquidity providers',
+                link:'provider'
+            },
+            {
+                image:'content/img/homepage_solution/3.png',
+                title:'Floating leverage',
+                text:'A service which provides customers with a flexible solution for the margin requirements',
+                link:'floating'
             },
             {
                 image:'content/img/homepage_solution/6.png',
-                title:'lorem',
-                text:'Lorem ipsum dolor sit amet, consectetur'
+                title:'Auto Risk Management',
+                text:'An intelligent system for automatic management of the company’s risks',
+                link:'auto_risk'
+            },
+            {
+                image:'content/img/homepage_solution/4 (2).png',
+                title:'API',
+                text:'A software interface which allows to trade without the trading terminal',
+                link:'api'
             }
         ]
 
